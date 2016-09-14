0f2da31daca1f01b6d3c7478b44d5bbc44bed614
Niall Gallagher <niall@npgall.com>
2016-09-14 01:15:16 +0100

    Initial upload of source.

---
+++ code/cqengine-stream-support/src/main/java/com/googlecode/cqengine/query/StreamFactory.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2012-2016 Niall Gallagher
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.cqengine.query;
+
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+/**
+ * A static factory for converting {@code java.lang.Iterable}s into Java 8 {@code java.util.stream.Stream}s.
+ * <p/>
+ * This is primarily for use with CQEngine {@code ResultSet}s, as a means to allow CQEngine to retain compatibility
+ * with Java 6, while it may be used with this library which works with Java 8.
+ *
+ * @author Niall Gallagher
+ */
+@SuppressWarnings("WeakerAccess")
+public class StreamFactory {
+
+    public static <O> Stream<O> streamOf(Iterable<O> iterable) {
+        return StreamSupport.stream(iterable.spliterator(), false);
+    }
+
+    /**
+     * Private constructor, unused.
+     */
+    StreamFactory() {
+    }
+}
---
+++ code/cqengine-stream-support/src/test/java/com/googlecode/cqengine/query/StreamFactoryTest.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2012-2016 Niall Gallagher
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.cqengine.query;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit tests for {@link StreamFactory}.
+ *
+ * @author Niall Gallagher
+ */
+public class StreamFactoryTest {
+
+    @Test
+    public void testStreamOf() {
+        List<Integer> input = Arrays.asList(1, 2, 3);
+        Stream<Integer> stream = StreamFactory.streamOf(input);
+        List<Integer> output = stream.collect(Collectors.toList());
+        Assert.assertEquals(input, output);
+    }
+
+    @Test
+    public void testPrivateConstructor() {
+        Assert.assertNotNull(new StreamFactory());
+    }
+}
